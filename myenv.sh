1e8477ddb46753f256f4e371b244bdd578cf068f
Paul Miller <paul@voltar.org>
2014-05-01 07:25:37 -0400

    no debug

--- myenv.sh
+++ myenv.sh
@@ -75,7 +75,8 @@ case "${cmd:-test}" in
 
     config*)
         $0 clean
-        sh ./configure --enable-debug --build=i686 --target=arm-linux --host=arm-linux "$@"
+        #h ./configure --enable-debug --build=i686 --target=arm-linux --host=arm-linux "$@"
+        sh ./configure --build=i686 --target=arm-linux --host=arm-linux "$@"
         ;;
 
     test)
